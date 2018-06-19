32cc41d91f4c06cfdbcba9a3449a2f3eb1f703d3
asscherhofstede <asscherhofstede@hotmail.com>
2018-06-19 22:18:37 +0200

    firstcommitcalculator

---
+++ calstack/calstack/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace calstack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtBox1 = new System.Windows.Forms.TextBox();
+            this.TxtBox2 = new System.Windows.Forms.TextBox();
+            this.MyStack = new System.Windows.Forms.RadioButton();
+            this.ArrayStack = new System.Windows.Forms.RadioButton();
+            this.MyListStack = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // TxtBox1
+            // 
+            this.TxtBox1.Location = new System.Drawing.Point(57, 72);
+            this.TxtBox1.Name = "TxtBox1";
+            this.TxtBox1.Size = new System.Drawing.Size(190, 22);
+            this.TxtBox1.TabIndex = 0;
+            // 
+            // TxtBox2
+            // 
+            this.TxtBox2.Location = new System.Drawing.Point(443, 12);
+            this.TxtBox2.Multiline = true;
+            this.TxtBox2.Name = "TxtBox2";
+            this.TxtBox2.Size = new System.Drawing.Size(345, 426);
+            this.TxtBox2.TabIndex = 1;
+            // 
+            // MyStack
+            // 
+            this.MyStack.AutoSize = true;
+            this.MyStack.Location = new System.Drawing.Point(319, 45);
+            this.MyStack.Name = "MyStack";
+            this.MyStack.Size = new System.Drawing.Size(82, 21);
+            this.MyStack.TabIndex = 5;
+            this.MyStack.TabStop = true;
+            this.MyStack.Text = "MyStack";
+            this.MyStack.UseVisualStyleBackColor = true;
+            // 
+            // ArrayStack
+            // 
+            this.ArrayStack.AutoSize = true;
+            this.ArrayStack.Location = new System.Drawing.Point(319, 72);
+            this.ArrayStack.Name = "ArrayStack";
+            this.ArrayStack.Size = new System.Drawing.Size(98, 21);
+            this.ArrayStack.TabIndex = 6;
+            this.ArrayStack.TabStop = true;
+            this.ArrayStack.Text = "ArrayStack";
+            this.ArrayStack.UseVisualStyleBackColor = true;
+            // 
+            // MyListStack
+            // 
+            this.MyListStack.AutoSize = true;
+            this.MyListStack.Location = new System.Drawing.Point(319, 99);
+            this.MyListStack.Name = "MyListStack";
+            this.MyListStack.Size = new System.Drawing.Size(104, 21);
+            this.MyListStack.TabIndex = 7;
+            this.MyListStack.TabStop = true;
+            this.MyListStack.Text = "MyListStack";
+            this.MyListStack.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MyListStack);
+            this.Controls.Add(this.ArrayStack);
+            this.Controls.Add(this.MyStack);
+            this.Controls.Add(this.TxtBox2);
+            this.Controls.Add(this.TxtBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtBox1;
+        private System.Windows.Forms.TextBox TxtBox2;
+        private System.Windows.Forms.RadioButton MyStack;
+        private System.Windows.Forms.RadioButton ArrayStack;
+        private System.Windows.Forms.RadioButton MyListStack;
+    }
+}
+
---
+++ calstack/calstack/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calstack
+{
+    public partial class Form1 : Form
+    {
+        Point locationNumbers = new Point(20, 265);
+        Point locationOperators = new Point(20, 265);
+        string input = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            ClientSize = new Size(800, 450);
+            LoadButtons();
+            LoadOperators();
+        }
+        /// <summary>
+        /// Het laden van de buttons met de nummers
+        /// </summary>
+        private void LoadButtons()
+        {
+            for (int i = 10; i > 0; i--)
+            {
+                //voor de 0 button
+                if (i == 10)
+                {
+                    Button b = new Button();
+                    b.Size = new Size(50, 50);
+                    locationNumbers.Offset(b.Width + 5, 0);
+                    b.Text = Convert.ToString(10 - i);
+                    b.Click += button_Click;
+                    b.Location = locationNumbers;
+                    Controls.Add(b);
+                    locationNumbers.Offset((b.Width + 5) * -1, (b.Height + 5) * -1);
+                }
+                //voor de andere nummers
+                else
+                {
+                    Button b = new Button();
+                    b.Size = new Size(50, 50);
+                    b.Text = Convert.ToString(10 - i);
+                    b.Click += button_Click;
+                    b.Location = locationNumbers;
+                    locationNumbers.Offset(b.Width + 5, 0);
+                    Controls.Add(b);
+
+                    if (i == 7)
+                    {
+                        locationNumbers.Offset((b.Width * 3 + 5 * 3) * -1, (b.Height + 5) * -1);
+                    }
+                    if (i == 4)
+                    {
+                        locationNumbers.Offset((b.Width * 3 + 5 * 3) * -1, (b.Height + 5) * -1);
+                    }
+                }
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button ButtonThatWasPushed = (Button)sender;
+             //the button's Text
+                                                          //do something
+
+            //If you store the button's numeric value in it's Tag property
+            //things become even easier.
+            input += ButtonThatWasPushed.Text;
+            TxtBox1.Text = input;
+            
+        }
+
+        /// <summary>
+        /// Het laden van de buttons met de operators (+ - * / etc.)
+        /// </summary>
+        private void LoadOperators()
+        {
+            for (int i = 6; i > 0; i--)
+            {
+                Button b = new Button();
+                b.Size = new Size(50, 50);
+
+                if (i == 6)
+                {
+                    b.Text = "(-)";
+                    b.Click += button_Click_Minus;
+                    b.Location = locationOperators;
+                    Controls.Add(b);
+                    locationOperators.Offset((b.Width + 5) * 2, 0);
+                }
+                if (i == 5)
+                {
+                    b.Text = "+";
+                    b.Click += button_Click_Minus;
+                    b.Location = locationOperators;
+                    Controls.Add(b);
+                    locationOperators.Offset((b.Width + 5) , 0);
+                }
+                if (i == 4)
+                {
+                    b.Text = "=";
+                    b.Click += button_Click_Minus;
+                    b.Location = locationOperators;
+                    Controls.Add(b);
+                    locationOperators.Offset(0, (b.Height+5)*-1);
+                }
+                if( i == 3)
+                {
+                    b.Text = "*";
+                    b.Click += button_Click_Minus;
+                    b.Location = locationOperators;
+                    Controls.Add(b);
+                    locationOperators.Offset(0, (b.Height + 5) * -1);
+                }
+                if (i == 2)
+                {
+                    b.Text = "/";
+                    b.Click += button_Click_Minus;
+                    b.Location = locationOperators;
+                    Controls.Add(b);
+                    locationOperators.Offset(0, (b.Height + 5) * -1);
+                }
+                if (i == 1)
+                {
+                    b.Text = "=";
+                    b.Click += button_Click_Minus;
+                    b.Location = locationOperators;
+                    Controls.Add(b);
+                    locationOperators.Offset(0, (b.Height + 5) * -1);
+                }
+                
+                
+            }
+        }
+
+        private void button_Click_Minus(object sender, EventArgs e)
+        {
+            Button ButtonThatWasPushed = (Button)sender;
+            //the button's Text
+            //do something
+
+            //If you store the button's numeric value in it's Tag property
+            //things become even easier.
+            input = ButtonThatWasPushed.Text;
+            if (TxtBox1.Text == null)
+            {
+                
+            }
+            TxtBox1.Text += input;
+        }
+    }
+}
